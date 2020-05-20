120f436bfda72ec6131689534a817a2a46e5e8cd
v.omelchenko <v.omelchenko@smartweb.com.ua>
2020-05-20 21:54:24 +0300

    login page + router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import TodoList from './todoList';
+import { LoginBlock } from './authPage';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="main-container">
-      <header className="header">
 
+      <header className="header">
+          <div className={"logo"}>
+              Kisa's ToDo
+          </div>
       </header>
       <div className="content">
-        <TodoList />
+          <Switch>
+              <Route path='/login' component={LoginBlock} />
+              <Route path='/todo' component={TodoList} />
+          </Switch>
       </div>
     </div>
   );
